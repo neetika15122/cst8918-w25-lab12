1f62b9c6949fe48613e2bc159ff28ecc967a92f0
BhavikaPathria-02 <path0053@algonquinlive.com>
2025-04-01 18:42:02 -0400

    use_oidc commit

--- infra/tf-app/terraform.tf
+++ infra/tf-app/terraform.tf
@@ -4,6 +4,7 @@ terraform {
     storage_account_name = "path0053githubactions"
     container_name       = "tfstate"
     key                  = "prod.app.tfstate"
+    use_oidc             = true
   }
 
   required_version = ">= 1.3.0"
@@ -20,4 +21,5 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "5fac1c80-5422-4c76-933d-a10e479aa38b"
+  use_oidc             = true
 }
